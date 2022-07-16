640ad00736ede3a1b09dd456786390d8495a591a
Patrick <github@z33.ch>
2022-07-16 14:00:56 +0200

    Update run.py

--- run.py
+++ run.py
@@ -6,27 +6,27 @@ import time
 import os.path
 
 # User preferences
-opsheadless = 1 # 0 show browser during running, 1 run browser in headless mode
-opsnzzuser = "YOUR_USERNAME" # add your nzz.ch username
-opsnzzpwd =  "YOUR_PASSWORD" # add your nzz.ch password
-opsoutputpathfile = "CHOOSE_PATH" # path of the file you want to write the results into, add / at the end
-opsoutputnamefile = "CHOOSE_NAME" # name of the file you want to write the results into
+opheadless = 1 # 0 show browser during running, 1 run browser in headless mode
+opnzzuser = "YOUR_USERNAME" # add your nzz.ch username
+opnzzpwd =  "YOUR_PASSWORD" # add your nzz.ch password
+opoutputpathfile = "CHOOSE_PATH" # path of the file you want to write the results into, add / at the end
+opoutputnamefile = "CHOOSE_NAME" # name of the file you want to write the results into
 
 # System preferences, should not be changed unless you know what to do
-opsnzzloginsite = "https://login.nzz.ch/?target=https%3A%2F%2Fwww.nzz.ch%2F&action=login"
-opsnzzlistsite = "https://www.nzz.ch/meine-nzz/merkliste"
-opsnzzloginfield = "input#c1-login-field"
-opsnzzpwdfield = "input#c1-password-field"
-opsnzzmorebutton = "button.button--loadmore"
-opsnzztagname = "article"
-opsnumberclicks = 3
-opsoutputtypefile = ".csv"
-opslogtypefile = ".log"
-opsoutputfiletowrite = (opsoutputpathfile + opsoutputnamefile + opsoutputtypefile)
+opnzzloginsite = "https://login.nzz.ch/?target=https%3A%2F%2Fwww.nzz.ch%2F&action=login"
+opnzzlistsite = "https://www.nzz.ch/meine-nzz/merkliste"
+opnzzloginfield = "input#c1-login-field"
+opnzzpwdfield = "input#c1-password-field"
+opnzzmorebutton = "button.button--loadmore"
+opnzztagname = "article"
+opnumberclicks = 3
+opoutputtypefile = ".csv"
+oplogtypefile = ".log"
+opoutputfiletowrite = (opoutputpathfile + opoutputnamefile + opoutputtypefile)
 countartold = 0
 countartnew = 0
 countartfin = 1
-
+today = date.today()
 
 # Functions
 def addentry_tofile():
@@ -35,11 +35,22 @@ def addentry_tofile():
 def addheader_tofile():
     myfile.write("DATE,COUNT" + "\n")
 
+# Check first if output file doesn't include an entry for today already
+if os.path.exists(opoutputfiletowrite):
+    with open(opoutputfiletowrite) as myfile:
+        if str(today) in myfile.read():
+            print("Already an entry for today, exiting script")
+            exit()            
+        else:
+            pass
+else:
+    pass
+
 # Start script
 edgeop = webdriver.EdgeOptions()
-if opsheadless == 1:
+if opheadless == 1:
     edgeop.add_argument('headless')
-elif opsheadless == 0:
+elif opheadless == 0:
     pass
 else:
     print("Invalid options, please choose 0 or 1")
@@ -47,38 +58,37 @@ else:
 driver = webdriver.Edge(options=edgeop)
 
 # Login to site
-driver.get(opsnzzloginsite)
-elemuser = driver.find_element(By.CSS_SELECTOR, opsnzzloginfield)
+driver.get(opnzzloginsite)
+elemuser = driver.find_element(By.CSS_SELECTOR, opnzzloginfield)
 elemuser.clear()
-elemuser.send_keys(opsnzzuser)
-elempwd = driver.find_element(By.CSS_SELECTOR, opsnzzpwdfield)
+elemuser.send_keys(opnzzuser)
+elempwd = driver.find_element(By.CSS_SELECTOR, opnzzpwdfield)
 elempwd.clear()
-elempwd.send_keys(opsnzzpwd)
+elempwd.send_keys(opnzzpwd)
 elempwd.send_keys(Keys.RETURN)
 
 # Switch to read list and count number of articles
 time.sleep(5)
-driver.get(opsnzzlistsite)
+driver.get(opnzzlistsite)
 time.sleep(3)
-elemmore = driver.find_element(By.CSS_SELECTOR, opsnzzmorebutton)
-today = date.today()
+elemmore = driver.find_element(By.CSS_SELECTOR, opnzzmorebutton)
 while countartfin != countartnew:
     currentnumberofclicks = 0
-    while currentnumberofclicks < opsnumberclicks:
+    while currentnumberofclicks < opnumberclicks:
         elemmore.click()
         time.sleep(0.5)
         currentnumberofclicks += 1
     countartold = countartnew
-    countartnew = len(driver.find_elements(By.TAG_NAME, opsnzztagname))
+    countartnew = len(driver.find_elements(By.TAG_NAME, opnzztagname))
     countartfin = countartold
 
 # Write final number into file
-if os.path.exists(opsoutputfiletowrite):
-    with open(opsoutputfiletowrite, "+a") as myfile:
+if os.path.exists(opoutputfiletowrite):
+    with open(opoutputfiletowrite, "+a") as myfile:
         addentry_tofile()
 
 else:
-    with open(opsoutputfiletowrite, "a") as myfile:
+    with open(opoutputfiletowrite, "a") as myfile:
         addheader_tofile()
         addentry_tofile()
 
